2dea27e51d94a3d5aec7a784091a0357afa2c788
ELDIABE22 <jeronimo.jsa.133@gmail.com>
2023-11-16 12:13:39 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-med-reminder-app/',
   plugins: [react()],
-  base: 'https://ELDIABE22.github.io/my-med-reminder-app'
 })
